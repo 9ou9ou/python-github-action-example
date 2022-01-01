17783bbfa879b726cffba1f3f46edbbabd8f217b
9ou9ou <47674678+9ou9ou@users.noreply.github.com>
2022-01-01 19:14:08 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World!"
+
+@app.route("/wassim")
+def index():
+    return "Hello, Wassim!"
     
 if __name__ == "__main__":
    app.run()
